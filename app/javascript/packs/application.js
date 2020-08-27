fae55e60977050791b6da2ef56df1348a854e06c
Celine Mercier <mercier_celine@hotmail.fr>
2020-08-27 22:00:17 +0200

    front on graph page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,74 +27,15 @@ import "bootstrap";
 var Highcharts = require('highcharts');  
 // Load module after Highcharts is loaded
 require('highcharts/modules/exporting')(Highcharts);  
+import { makeCharts } from './charts';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-const allAnalysesContainer = document.querySelectorAll('.container')
-
-
-const makeCharts = () => {
-  const containerAnalyses = document.querySelector("#container-analyses")
-  const miniValues = containerAnalyses.dataset.minValue.split(', ');
-  const maxiValues = containerAnalyses.dataset.maxValue.split(', ');
-  const datesXAxis = containerAnalyses.dataset.dates.split(', ');
-  const values = containerAnalyses.dataset.values.split(', ');
-  const unit = containerAnalyses.dataset.unit;
-
-  let minValues = [];
-  miniValues.forEach(value => {
-    minValues.push(parseFloat(value));
-  });
-
-  let maxValues = [];
-  maxiValues.forEach(value => {
-    maxValues.push(parseFloat(value));
-  });
-
-  let finalData = [];
-  values.forEach(value => {
-    finalData.push(parseFloat(value));
-  });
-
-
-  Highcharts.chart('container-analyses', {
-    chart: {
-      type: 'line'
-    },
-    title: {
-      text: ""
-    },
-    xAxis: {
-      categories: datesXAxis
-    },
-    yAxis: {
-      title: {
-        text: unit
-      }
-    },
-    plotOptions: {
-      line: {
-        dataLabels: {
-          enabled: true
-        },
-        enableMouseTracking: false
-      }
-    },
-    series: [{
-      name: "Vos valeurs",
-      data: finalData
-    }, {
-      name: "Valeurs usuelles minimum",
-      data: minValues
-    }, {
-      name: "Valeurs usuelles maximum",
-      data: maxValues
-    }]
-  });
-};
-
 
 document.addEventListener('turbolinks:load', () => {
   makeCharts();
+  // document.querySelectorAll('#all-analyses').forEach(() => {
+  //   makeCharts
+  // });
 });

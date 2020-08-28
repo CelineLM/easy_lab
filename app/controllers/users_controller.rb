cfa30e84f333e289051b2137093e03af35d3b05e
samah giambiasi <samah.elkahlaoui@gmail.com>
2020-08-28 13:01:19 +0200

    Dashboard page html css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
     @checkup = Checkup.new
+    @user_analyses = current_user.user_analyses
+    @analyses_grouped_by_name = @user_analyses.group_by{ |user_analysis| user_analysis.name }.select { |name, instances| instances.size > 1 }
   end
 end

262a545e1dd79f07fa5582707d2a400a5ba976a9
Celine Mercier <mercier_celine@hotmail.fr>
2020-09-03 17:10:01 +0200

    graphs sorted by date.

--- app/controllers/user_analyses_controller.rb
+++ app/controllers/user_analyses_controller.rb
@@ -1,6 +1,8 @@
 class UserAnalysesController < ApplicationController
   def index
     @user_analyses = current_user.user_analyses
-    @analyses_grouped_by_name = @user_analyses.group_by{ |user_analysis| user_analysis.name }.select { |name, instances| instances.size > 1 } 
+    @analyses_grouped_by_name = @user_analyses.group_by { |user_analysis| user_analysis.name }.select { |_name, instances| instances.size > 1 }
+    @checkups = @analyses_grouped_by_name.map { |_key ,value| value.map { |analyse| analyse.checkup } }
+    @checkups.map {|type_checkup| type_checkup.sort! { |checkup, checkup2| checkup.realized_on <=> checkup2.realized_on }} 
   end
 end

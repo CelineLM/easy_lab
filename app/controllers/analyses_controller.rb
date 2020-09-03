79fba46626d5fabd21f5158c5e8cbf94988bcbca
samah giambiasi <samah.elkahlaoui@gmail.com>
2020-09-03 15:02:49 +0200

    add if else search

--- app/controllers/analyses_controller.rb
+++ app/controllers/analyses_controller.rb
@@ -13,13 +13,17 @@ class AnalysesController < ApplicationController
   def search
     if params[:search][:query].present?
       @analysis = Analysis.where("name ILIKE ?", "%#{params[:search][:query]}%").first
-      redirect_to analysis_path(@analysis)
+      if @analysis.blank?
+        flash[:alert] = "Nous n'avons pas trouver de correspondance à votre recherche"
+        redirect_to analyses_path
+      else
+        redirect_to analysis_path(@analysis)
+      end
     else
+      flash[:alert] = 'La recherche ne peut pas être vide!'
       redirect_to analyses_path
     end
   end
-
 end
-
 
 
